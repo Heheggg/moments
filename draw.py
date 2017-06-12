6650aaa98aab3450e6566ac9724d2c54feab67ed
Yueyang <yxu8@stuy.edu>
2017-06-12 19:22:15 +0100

    initial works

--- draw.py
+++ draw.py
@@ -8,18 +8,22 @@ def add_polygon( polygons, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
     add_point(polygons, x1, y1, z1);
     add_point(polygons, x2, y2, z2);
 
-def draw_polygons( matrix, screen, color ):
+def draw_polygons( matrix, screen,zb, color ):
     if len(matrix) < 2:
         print 'Need at least 3 points to draw'
         return
 
-    point = 0    
+    point = 0
+    r = 255
+    g = 255
+    b = 255
+    
     while point < len(matrix) - 2:
 
         normal = calculate_normal(matrix, point)[:]
         print normal
         if normal[2] > 0:
-            scanline(matrix)
+            scanline(matrix,screen,[r,g,b])
             draw_line( int(matrix[point][0]),
                        int(matrix[point][1]),
                        int(matrix[point+1][0]),
@@ -37,21 +41,65 @@ def draw_polygons( matrix, screen, color ):
                        screen, color)    
             point+= 3
 
-def scanline(matrix):
-    sorty = {matrix[0][1]:0,matrix[0][1]:1,matrix[0][2]:2}
+            r = (r + 31) % 256
+            g = (r + 109) % 256
+            b = (r + 199) % 256
+
+        point += 3
+        
+def scanline(matrix, screen, color):
+    sorty = {matrix[0][0]:0,matrix[0][1]:1,matrix[0][2]:2}
     output = sorted(sorty.keys())
     p_low = matrix[sorty[output[0]]]
     p_mid = matrix[sorty[output[1]]]
-    p_high = matrix[sorty[output[2]]]
-
+    p_top = matrix[sorty[output[2]]]
+    
 
     y0 = p_low[1]
     x0 = p_low[0]
-    
-    while y_low < y_high:
-        drawline(x0,y0,)
+    d_x0 = ((1.0*(p_top[0]-p_low[0]))/(p_top[1]-p_low[1]))
+    d_z0 = ((1.0*(p_top[2]-p_low[2]))/(1.0*(p_top[1]-p_low[1])))
 
-def scancheck():
+    if p_low[1] != p_mid[1]:
+        d_x1 = ((1.0*(p_mid[0]-p_low[0]))/(1.0*(p_mid[1]-p_low[1])))
+        d_z1 = ((1.0*(p_mid[2]-p_low[2]))/(1.0*(p_mid[1]-p_low[1])))
+        x1 = p_low[0]
+        z1 = p_low[2]
+    else:
+        d_x1 = ((1.0*(p_top[0]-p_low[0]))/(1.0*(p_top[1]-p_low[1])))
+        d_z1 = ((1.0*(p_top[2]-p_low[2]))/(1.0*(p_top[1]-p_low[1])))
+        x1 = p_mid[0]
+        z1 = p_mid[2]
+
+    y = p_low[1]
+    i = 0
+    while y < p_top[1]:
+        drawline(int(x0),int(y),int(z0),int(x1),int(y), int(z1), screen, zbuffer, color)
+
+        if(y < p_mid[1] and p_mid[1]-y <1):
+            
+            x0 = p_low[0] + (p_mid[1]-p_low[1])*d_x0
+            x1 = p_mid[0]
+            y = p_mid[1]
+            z0 = p_low[2] + (p_mid[1]-p_low[1])*d_z0
+            z1 = p_mid[2]
+            drawline(int(x0),int(y),int(z0),int(x1),int(y), int(z1), screen, zbuffer, color)
+
+        if y == p_mid[1]:
+            x1 = p_mid[0]
+            z1 = p_mid[2]
+            if p_top[1] != p_mid[1]:
+                d_x1 = ((1.0*(p_top[0]-p_mid[0]))/(1.0*(p_top[1]-p_mid[1])))
+                d_z1 = ((1.0*(p_top[2]-p_low[2]))/(1.0*(p_top[1]-p_mid[1])))
+            else:
+                d_x1 = ((1.0*(p_top[0]-p_low[0]))/(1.0*(p_top[1]-p_low[1])))
+                d_z1 = ((1.0*(p_top[2]-p_low[2]))/(1.0*(p_top[1]-p_low[1])))
+
+        x0 += d_x0
+        x1 += d_x1
+        y += 1
+        z0 += d_z0
+        z1 += d_z1
 
 def add_box( polygons, x, y, z, width, height, depth ):
     x1 = x + width
@@ -255,89 +303,3 @@ def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
 def add_point( matrix, x, y, z=0 ):
     matrix.append( [x, y, z, 1] )
     
-
-
-
-def draw_line( x0, y0, x1, y1, screen, color ):
-
-    #swap points if going right -> left
-    if x0 > x1:
-        xt = x0
-        yt = y0
-        x0 = x1
-        y0 = y1
-        x1 = xt
-        y1 = yt
-
-    x = x0
-    y = y0
-    A = 2 * (y1 - y0)
-    B = -2 * (x1 - x0)
-
-    #octants 1 and 8
-    if ( abs(x1-x0) >= abs(y1 - y0) ):
-
-        #octant 1
-        if A > 0:            
-            d = A + B/2
-
-            while x < x1:
-                plot(screen, color, x, y)
-                if d > 0:
-                    y+= 1
-                    d+= B
-                x+= 1
-                d+= A
-            #end octant 1 while
-            plot(screen, color, x1, y1)
-        #end octant 1
-
-        #octant 8
-        else:
-            d = A - B/2
-
-            while x < x1:
-                plot(screen, color, x, y)
-                if d < 0:
-                    y-= 1
-                    d-= B
-                x+= 1
-                d+= A
-            #end octant 8 while
-            plot(screen, color, x1, y1)
-        #end octant 8
-    #end octants 1 and 8
-
-    #octants 2 and 7
-    else:
-        #octant 2
-        if A > 0:
-            d = A/2 + B
-
-            while y < y1:
-                plot(screen, color, x, y)
-                if d < 0:
-                    x+= 1
-                    d+= A
-                y+= 1
-                d+= B
-            #end octant 2 while
-            plot(screen, color, x1, y1)
-        #end octant 2
-
-        #octant 7
-        else:
-            d = A/2 - B;
-
-            while y > y1:
-                plot(screen, color, x, y)
-                if d > 0:
-                    x+= 1
-                    d+= A
-                y-= 1
-                d-= B
-            #end octant 7 while
-            plot(screen, color, x1, y1)
-        #end octant 7
-    #end octants 2 and 7
-#end draw_line
